32076183674bbd22bfd6b4147bfbb6e59f5c8992
unknown <shahuashita@gmail.com>
2024-07-20 22:51:00 +0530

    added the wishlist functionality and fixed the product page issues

--- Modesons/pages/productDescriptionPage/productDescriptionPage.js
+++ Modesons/pages/productDescriptionPage/productDescriptionPage.js
@@ -1,32 +1,12 @@
-const wishlistBtn = document.getElementById("wishlistBtn");
-const womenURL = 'https://modesense-data.onrender.com/womenproducts';
+const wishlistBtn = document.querySelector(".wishlistBtn");
 
-wishlistBtn.addEventListener("click",()=>{
+wishlistBtn.addEventListener("click", () => {
 
-  fetch('womenURL')
-    .then(response => response.json())
-    .then(products => {
-        
-        wishlistBtn.addEventListener("click", () => {
-            //let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+    alert("Product added to wishlist");
+    
 
-            const productExists = wishlist.some(item => item.id === id);
+    window.location.href = "./wishlist.html";
+});
 
-            if (productExists) {
-                alert("Product is already added to the wishlist.");
-            } else {
-                // wishlist.push(product);
-                localStorage.setItem('wishlist', JSON.stringify(wishlist));
-                //console.log(product);
 
-                window.location.href = "/Freedom-Frameworks_025-/Modesons/pages/wishlist/wishlist.html";
-            }
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching product data:', error);
-    });
-})
-
-// Fetch product data from the API
 
